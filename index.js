0082082a956f9c4a75eec002717c6e8ea1f45009
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-02 15:04:57 -0400

    commit tree 630ec9219e859ba3f498bbd384db676a959886f5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str){
+    return drivers.filter(word => word.toLowerCase() == str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(word => word.startsWith(str));
+}
+
+function matchName(drivers, str){
+    return drivers.filter(element => element.name == str)
+}
